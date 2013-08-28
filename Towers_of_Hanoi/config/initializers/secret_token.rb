4ce2b86d70b1aecc2cd8de6afe2b1032910081e3
Wesley Carr <surfswell17@gmail.com>
2013-08-28 13:48:49 -0400

    init

---
+++ Towers_of_Hanoi/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TowersOfHanoi::Application.config.secret_key_base = '5a5c3023296f243b49f1830a2d70cdcbc205cf84a2fd8b1043c1601ab84fc230c72b51301e37c8cd97d221f65e547804a1775e9cb4d5b29543c9a81ae054b774'
